35e55ba34e7ccc5ba4cbc8e9bd6e0fbc9fd60fb4
se42 <scott@scottmedwards.com>
2016-11-06 18:54:43 +0000

    html hello world page

--- startup.js
+++ startup.js
@@ -1,7 +1,14 @@
 var http = require('http');
+var fs = require('fs');
 var port = process.argv[2];
 
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World!\n');
+  fs.readFile(__dirname + '/hello_world.html', (error, htmlContent) => {
+    if(error) {
+      response.writeHead(500, {'Content-Type': 'text/html'});
+      response.end('<h1>500</h1><p>Sorry...</p>');
+    }
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.end(htmlContent);
+  });
 }).listen(port, 'localhost');
