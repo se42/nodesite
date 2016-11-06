e47bf887c6302fc4306f930a48f5d6aabf85b848
se42 <scott@scottmedwards.com>
2016-11-06 21:37:47 +0000

    basic static files configuration

--- startup.js
+++ startup.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var argv = require('minimist')(process.argv.slice(2));
 
 http.createServer(function (request, response) {
-  fs.readFile(__dirname + '/hello_world.html', (error, htmlContent) => {
+  fs.readFile(__dirname + '/html/hello_world.html', (error, htmlContent) => {
     if(error) {
       response.writeHead(500, {'Content-Type': 'text/html'});
       response.end('<h1>500</h1><p>Sorry...</p>');
