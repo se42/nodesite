419499f21328a2bceb768396ead0621f6ad3061a
se42 <scott@scottmedwards.com>
2016-11-06 18:17:49 +0000

    rename startup file

--- startup.js
+++ startup.js
@@ -1,9 +1,7 @@
 var http = require('http');
+var port = process.argv[2];
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
   response.end('Hello World!\n');
-}).listen(8081, 'localhost');
-
-console.log('Server running at http://localhost:8081/');
-
+}).listen(port, 'localhost');
