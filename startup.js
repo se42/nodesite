d88929be9c27684753c90d6500ce691d12bbbc56
se42 <scott@scottmedwards.com>
2016-12-20 01:55:29 +0000

    Installed basic sidebar theme from StartBootstrap; built out super basic structure/summary

--- startup.js
+++ startup.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var argv = require('minimist')(process.argv.slice(2));
 
 http.createServer(function (request, response) {
-  fs.readFile(__dirname + '/html/hello_world.html', (error, htmlContent) => {
+  fs.readFile(__dirname + '/html/home.html', (error, htmlContent) => {
     if(error) {
       response.writeHead(500, {'Content-Type': 'text/html'});
       response.end('<h1>500</h1><p>Sorry...</p>');
