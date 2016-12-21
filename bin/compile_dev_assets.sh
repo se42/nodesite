4c532024353b004929e5cb4686bb93c7647374fe
Scott Edwards <scott.edwards3@gmail.com>
2016-12-20 22:01:26 -0500

    add assets/img directory and update scripts to handle it

--- bin/compile_dev_assets.sh
+++ bin/compile_dev_assets.sh
@@ -1,1 +1,2 @@
 lessc /home/Scott/dev_scottmedwards_app/less/main.less /var/www/dev_scottmedwards_app/css/main.css
+cp /home/Scott/dev_scottmedwards_app/assets/img/* /var/www/dev_scottmedwards_app/img/
--- bin/compile_local_assets.sh
+++ bin/compile_local_assets.sh
@@ -1,1 +1,2 @@
 lessc /Users/Scott/projects/NimbusNode/less/main.less /Users/Scott/projects/dev_web_roots/NimbusNode/css/main.css
+cp /Users/Scott/projects/NimbusNode/assets/img/* /Users/Scott/projects/dev_web_roots/NimbusNode/img/
