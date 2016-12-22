04d06d0f5f1dad1a13208525134b8136b6ad8f6e
Scott Edwards <scott.edwards3@gmail.com>
2016-12-21 22:00:27 -0500

    Configure Pug templates for inheritance

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ var app = express();
 app.set('view engine', 'pug');
 app.set('views', './views');
 app.get('/', (req, res) => res.render('base'));
-app.get('/me', (req, res) => res.send("<p>Stuff about me</p>"));
-app.get('/site', (req, res) => res.send("<p>Information about how I built this site, tools used, etc.</p>"));
+app.get('/me', (req, res) => res.render('about_me'));
+app.get('/site', (req, res) => res.render('about_site'));
 
 app.listen(argv.port, function() {
     console.log(`App listening on port ${argv.port}`);
