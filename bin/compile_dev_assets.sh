8d6e74f5512717c8ccee7d2be7aead47e7e0e1ba
Scott Edwards <scott.edwards3@gmail.com>
2016-12-26 09:31:09 -0500

    Add favicon and configure scripts to deploy it

--- bin/compile_dev_assets.sh
+++ bin/compile_dev_assets.sh
@@ -1,3 +1,4 @@
 lessc /home/Scott/dev_scottmedwards_app/public/less/main.less /var/www/dev_scottmedwards_app/css/main.css
 cp /home/Scott/dev_scottmedwards_app/public/img/* /var/www/dev_scottmedwards_app/img/
 cp /home/Scott/dev_scottmedwards_app/public/js/* /var/www/dev_scottmedwards_app/js/
+cp /home/Scott/dev_scottmedwards_app/public/favicon.ico /var/www/dev_scottmedwards_app/favicon.ico
--- bin/deploy_app_to_master.sh
+++ bin/deploy_app_to_master.sh
@@ -9,4 +9,5 @@ npm install
 lessc /home/Scott/scottmedwards_app/public/less/main.less /var/www/scottmedwards_app/css/main.css
 cp /home/Scott/scottmedwards_app/public/img/* /var/www/scottmedwards_app/img/
 cp /home/Scott/scottmedwards_app/public/js/* /var/www/scottmedwards_app/js/
+cp /home/Scott/scottmedwards_app/public/favicon.ico /var/www/scottmedwards_app/favicon.ico
 pm2 restart app
