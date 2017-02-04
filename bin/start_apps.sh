1f93b830b836e2679aae975c4e4da742034cedfe
se42 <scott@scottmedwards.com>
2017-02-04 13:46:47 +0000

    Fix start script so apps start with correct cwd

--- bin/start_apps.sh
+++ bin/start_apps.sh
@@ -1,2 +1,5 @@
-pm2 start /home/Scott/dev_scottmedwards_app/main.js --name dev_app -- --port 8081
-pm2 start /home/Scott/scottmedwards_app/main.js --name app -- --port 8080
+cd /home/Scott/dev_scottmedwards_app
+pm2 start main.js --name dev_app -- --port 8081
+
+cd /home/Scott/scottmedwards_app
+pm2 start main.js --name app -- --port 8080
